66a11bd13f6203a51e6600f181f1b54d3ef9c1a1
Raziel Miranda <raziel.miranda@galata.com.br>
2019-10-01 23:06:15 +0100

    quase lá

--- src/HelloSample.java
+++ src/HelloSample.java
@@ -1,12 +1,23 @@
+import java.lang.reflect.Array;
+
 public class HelloSample {
+
+    static void funcao(String letras) {
+        System.out.println("teste da funcao = " + letras);
+    }
+
+    static  void metodo(){
+        System.out.println("Oi eu sou o metodo!");
+    }
+
 
     public static void main(String[] args){
 
         int x = 10;
         int y = 87;
-        int z = 0;
-
-        z = x+y;
+        int contador = 0;
+        int[] vetornumeros = { 1 , 2, 4, 59273};
+        int z = x + y;
 
         if(z > 97){
             System.out.println("z é maior que 97");
@@ -14,7 +25,20 @@ public class HelloSample {
             System.out.println("z é menor que 97");
         }
 
-        
+        while(contador < 10){
+            contador++;
+            System.out.println(contador);
+        }
+
+        for (contador = 0; contador < vetornumeros.length; contador++){
+            System.out.println(vetornumeros[contador]);
+        }
+
+        System.out.println(vetornumeros[3]);
+        String letras = "eu sou varias letras";
+
+        funcao("Estou sendo passado de parametro para a função :)");
+        metodo();
     }
 
 }
