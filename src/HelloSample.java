0c4dd7f0b96a23ebc2c0196361ffa452067e3156
Raziel Miranda <raziel.miranda@galata.com.br>
2019-10-01 22:27:58 +0100

    desenferujando do JAVA

---
+++ src/HelloSample.java
@@ -0,0 +1,20 @@
+public class HelloSample {
+
+    public static void main(String[] args){
+
+        int x = 10;
+        int y = 87;
+        int z = 0;
+
+        z = x+y;
+
+        if(z > 97){
+            System.out.println("z é maior que 97");
+        }else{
+            System.out.println("z é menor que 97");
+        }
+
+        
+    }
+
+}
